5dcd562f4a264f1df21cf7f5bdd8dec54983fd54
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-01-14 09:06:37 -0500

    Agregar archivos de proyecto.

---
+++ ListaClientesAPI/Controllers/TelefoniaController.cs
@@ -0,0 +1,40 @@
+﻿using ListaClientesAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaClientesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelefoniaController : ControllerBase
+    {
+        private static List<Telefonia> _telefonia = new List<Telefonia>
+        {
+            new Telefonia
+            {
+               Id = 1,
+               Nombre = "Claro",
+               Servicios = ServiciosAOfrecer.MinutosMegasInternet
+            },
+            new Telefonia
+            {
+               Id = 2,
+               Nombre = "Movistar",
+               Servicios = ServiciosAOfrecer.MinutosMegasInternetServiciosAdicionales
+            },
+            new Telefonia
+            {
+               Id = 3,
+               Nombre = "Tuenti",
+               Servicios = ServiciosAOfrecer.MinutosMegas
+            }
+        };
+        [HttpGet]
+        public IEnumerable<Telefonia> GetAllTelefonia()
+        {
+            return _telefonia;
+        }
+
+    }
+    
+}
